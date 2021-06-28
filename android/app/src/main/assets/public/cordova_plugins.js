a9a49a2afd75876345d3ab873db035052beb4ba7
cokcok <wattanai@rubber.mail.go.th>
2021-06-28 21:44:11 +0700

    main

---
+++ android/app/src/main/assets/public/cordova_plugins.js
@@ -0,0 +1,90 @@
+
+  cordova.define('cordova/plugin_list', function(require, exports, module) {
+    module.exports = [
+      {
+          "id": "cordova-plugin-camera.Camera",
+          "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+          "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+          "Camera"
+        ]
+        },
+      {
+          "id": "cordova-plugin-camera.CameraPopoverHandle",
+          "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
+          "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+          "CameraPopoverHandle"
+        ]
+        },
+      {
+          "id": "cordova-plugin-camera.CameraPopoverOptions",
+          "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+          "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+          "CameraPopoverOptions"
+        ]
+        },
+      {
+          "id": "cordova-plugin-app-version.AppVersionPlugin",
+          "file": "plugins/cordova-plugin-app-version/www/AppVersionPlugin.js",
+          "pluginId": "cordova-plugin-app-version",
+        "clobbers": [
+          "cordova.getAppVersion"
+        ]
+        },
+      {
+          "id": "cordova-plugin-inappbrowser.inappbrowser",
+          "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+          "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+          "cordova.InAppBrowser.open"
+        ]
+        },
+      {
+          "id": "cordova-plugin-file-opener2.FileOpener2",
+          "file": "plugins/cordova-plugin-file-opener2/www/plugins.FileOpener2.js",
+          "pluginId": "cordova-plugin-file-opener2",
+        "clobbers": [
+          "cordova.plugins.fileOpener2"
+        ]
+        },
+      {
+          "id": "cordova-plugin-device.device",
+          "file": "plugins/cordova-plugin-device/www/device.js",
+          "pluginId": "cordova-plugin-device",
+        "clobbers": [
+          "device"
+        ]
+        },
+      {
+          "id": "cordova-plugin-camera.camera",
+          "file": "plugins/cordova-plugin-camera/www/Camera.js",
+          "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+          "navigator.camera"
+        ]
+        },
+      {
+          "id": "cordova-plugin-statusbar.statusbar",
+          "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+          "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+          "window.StatusBar"
+        ]
+        }
+    ];
+    module.exports.metadata =
+    // TOP OF METADATA
+    {
+      "cordova-plugin-app-version": "0.1.12",
+      "cordova-plugin-camera": "4.1.0",
+      "cordova-plugin-device": "2.0.2",
+      "cordova-plugin-file-opener2": "3.0.5",
+      "cordova-plugin-inappbrowser": "4.1.0",
+      "cordova-plugin-statusbar": "2.4.2",
+      "cordova-plugin-whitelist": "1.3.3"
+    };
+    // BOTTOM OF METADATA
+    });
+    
